afb52b81b006b0ac834a2cd3565aa5e5d22bfbb6
mattzilles <matt.zilles@yahoo.com>
2022-04-21 21:27:05 -0400

    changes to account

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'account'
+
 urlpatterns = [
-    path('', views.login, name='login'), 
+    path('login/', views.login, name='login'), 
     path('register/', views.register, name='register'), 
+    path('register/request/', views.registerRequest, name='registerRequest')
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
 def login(request):
     return render(request, 'account/login.html', {})
 
 def register(request):
     return render(request, 'account/register.html', {})
+
+def registerRequest(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    firstName = request.POST['firstName']
+    lastName = request.POST['lastName']
+    email = request.POST['email']
+    user = User.objects.create_user(username, email, password)
+    user.first_name = firstName
+    user.last_name = lastName
+    user.save()
+    return HttpResponseRedirect('/account/login')
