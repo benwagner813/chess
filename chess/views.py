d905fb698f975a905faf742bc8edd21510b1dcb4
mattzilles <matt.zilles@yahoo.com>
2022-07-06 16:58:57 -0400

    A lot of progress on chess board, SVGs, web-sockets, and more

--- chess/views.py
+++ chess/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Chess
 
 
 def chessLobby(request):
     return render(request, "chess/chessLobby.html", {})
 
 def chessGame(request, id):
-    return render(request, "chess/chess.html", {})
+    return render(request, "chess/chess.html", {
+        'id' : id,
+    })
 
 def createGame(request):
-    pass
+    game = Chess()
+    game.save()
+    return redirect("chess:ChessGame", id = game.id)
